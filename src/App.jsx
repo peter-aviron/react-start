60cb7f19127c0579cbb2ef782d4244b740ce5f61
Peter Mayor <peter@avironsoftware.com>
2022-06-09 16:38:30 -0400

    update with todo code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import TodoList from "./TodoList";
 
-const App = () => {
-  return <h1>Create React App Without CRA ☘️</h1>;
-};
+function App() {
+  const [items, setItems] = React.useState([]);
+  const [text, setText] = React.useState("");
+
+  function handleClick() {
+    if (text.length === 0) {
+      return;
+    }
+    const newItem = {
+      text: text,
+      id: Date.now(),
+    };
+    setItems((prevItems) => {
+      return prevItems.concat(newItem);
+    });
+    setText("");
+  }
+
+  function handleChange(e) {
+    setText(e.target.value);
+  }
+
+  return (
+    <div>
+      <h3>TODO</h3>
+      <TodoList items={items} />
+      <label>What needs to be done?</label>
+      <input onChange={handleChange} value={text} />
+      <button onClick={handleClick}>Add #{items.length + 1}</button>
+    </div>
+  );
+}
 
 export default App;
